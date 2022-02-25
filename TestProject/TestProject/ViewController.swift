9a4cc0aac4d67bcff120be0cc2a48564a6eb1009
faiqadi <faiq.adiwibowo@gmail.com>
2022-02-26 02:17:29 +0700

    initiate file

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by faiq adi on 26/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestProject/TestProject/model/UserModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserModel.swift
+//  TestProject
+//
+//  Created by faiq adi on 26/02/22.
+//
+
+import Foundation
+
+struct userModel {
+    
+}
+"id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address"
